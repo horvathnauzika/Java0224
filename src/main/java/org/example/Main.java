22e7972b90d5a4ea0b98a89ed3c51d267d17e0a8
horvathnauzika <naihorvath@gmail.com>
2025-02-24 10:55:36 +0100

    teszteles elott

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import org.example.modell.Jarmu;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Járművek: ");
         Garazs garazs = new Garazs();
         for(Jarmu jarmu:garazs){
             System.out.println(jarmu);
